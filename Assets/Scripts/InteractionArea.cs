fe423d615ee9af7fd248bcfa3bf582000a7a7b8d
santiwinny <santiwinny@gmail.com>
2026-05-04 10:35:59 -0300

    Agregue la suma del puntaje

--- Assets/Scripts/InteractionArea.cs
+++ Assets/Scripts/InteractionArea.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class InteractionArea : MonoBehaviour
 {
+    public ScoreUiManager scoreUIMgr;
+
     void OnCollisionEnter(Collision col)
     {
         if(col.gameObject.CompareTag("Pickable"))
         {
            Debug.Log("Hubo colisión con un Pickable");
-           Destroy(col.gameObject); 
+           Destroy(col.gameObject);
+           scoreUIMgr.AddPointAndUpdateScoreUI(); 
         }
     }
 }
---
+++ Assets/Scripts/ScoreUiManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreUiManager : MonoBehaviour
+{
+    public int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void AddPointAndUpdateScoreUI()
+    {
+        score ++;
+        Debug.Log("Puntaje: " + score);
+    }
+}
