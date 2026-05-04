2bc5112301040c9d5f824b59c48eb551c50fb73b
santiwinny <santiwinny@gmail.com>
2026-05-04 10:09:58 -0300

    Initial commit

---
+++ Assets/Scripts/InteractionArea.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionArea : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Hubo colisión");
+    }
+}
